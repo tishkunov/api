21b4a15a8d123854c974b837f67caece02ed2e74
Constantine <generali8@mail.ru>
2018-07-19 13:22:30 +0300

    Update api.js

--- api.js
+++ api.js
@@ -98,11 +98,4 @@ app.get('/listUsers', function (req, res) {
 	});
 })
 
-var server = app.listen(22, function () {
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-
-})
